b66c304c1af5f050d778b522ebf31dc71f86a33e
niko <me@nikolasgoebel.com>
2018-07-27 16:45:37 +0200

    Fix RGA example

--- examples/rga.clj
+++ examples/rga.clj
@@ -1,65 +1,56 @@
 (ns clj-3df.examples.rga
   (:require
    [clj-3df.core :refer [create-conn create-db exec!
-                         register-plan register-query transact]]))
+                         register-plan register-query transact]]
+   [manifold.stream :as stream]
+   [manifold.bus :as bus]))
 
 ;; RGA
 ;; https://speakerdeck.com/ept/data-structures-as-queries-expressing-crdts-using-datalog?slide=22
 
 (def schema
-  {:id/node       {:db/valueType :Number}
-   :id/ctr        {:db/valueType :Number}
-   :insert/id     {:db/valueType :Eid}
-   :insert/parent {:db/valueType :Eid}
-   :assign/id     {:db/valueType :Eid}
-   :assign/elem   {:db/valueType :Eid}
-   :assign/value  {:db/valueType :String}
-   :remove/id     {:db/valueType :Eid}})
+  {:insert/after {:db/valueType :Eid}
+   :assign/elem  {:db/valueType :Eid}
+   :assign/value {:db/valueType :String}
+   :remove?      {:db/valueType :Bool}})
 
 (def db (create-db schema))
 
 (def rules
-  '[[(has-child? ?parent) [?wc :insert/parent ?parent]]
+  '[[(has-child? ?parent) [?wc :insert/after ?parent]]
 
     [(later-child ?parent ?child2)
-     [?i1 :insert/parent ?parent] [?i1 :insert/id ?child1]
-     [?i2 :insert/parent ?parent] [?i2 :insert/id ?child2]
-     (or
-      (and
-       [?child1 :id/ctr ?ctr1] [?child2 :id/ctr ?ctr2]
-       [(> ?ctr1 ?ctr2)])
-      (and
-       [?child1 :id/ctr ?ctr] [?child1 :id/node ?n1]
-       [?child2 :id/ctr ?ctr] [?child2 :id/node ?n2]
-       [(> ?n1 ?n2)]))]
+     [?child1 :insert/after ?parent]
+     [?child2 :insert/after ?parent]
+     [(> ?child1 ?child2)]]
 
     [(first-child ?parent ?child)
-     [?i :insert/id ?child] [?i :insert/parent ?parent]
+     [?child :insert/after ?parent]
      (not (later-child ?parent ?child))]
 
     [(sibling? ?child1 ?child2)
-     [?i1 :insert/id ?child1] [?i1 :insert/parent ?parent]
-     [?i2 :insert/id ?child2] [?i2 :insert/parent ?parent]]
+     [?child1 :insert/after ?parent]
+     [?child2 :insert/after ?parent]]
 
     [(later-sibling ?sib1 ?sib2)
      (sibling? ?sib1 ?sib2)
-     (later? ?sib1 ?sib2)]
+     [(> ?sib1 ?sib2)]]
 
     [(later-sibling2 ?sib1 ?sib3)
      (sibling? ?sib1 ?sib2)
      (sibling? ?sib1 ?sib3)
-     (later? ?sib1 ?sib2)
-     (later? ?sib2 ?sib3)]
+     [(> ?sib1 ?sib2)]
+     [(> ?sib2 ?sib3)]]
 
     [(next-sibling ?sib1 ?sib2)
      (later-sibling ?sib1 ?sib2)
-     (not (later-sibling ?sib1 ?sib2))]
+     (not (later-sibling2 ?sib1 ?sib2))]
 
     [(has-next-sibling? ?sib1) (later-sibling ?sib1 ?wc)]
 
     [(next-sibling-anc ?start ?next) (next-sibling ?start ?next)]
     [(next-sibling-anc ?start ?next)
-     [?i1 :insert/id ?start] [?i1 :insert/parent ?parent]
+     [?start :insert/after ?parent]
      (next-sibling-anc ?parent ?next)
      (not (has-next-sibling? ?start))]
 
@@ -71,8 +62,9 @@
     ;; Assigning values to list elements.
 
     [(current-value ?elem ?value)
-     [?op :assign/id ?id] [?op :assign/elem ?elem] [?op :assign/value ?value]
-     (not [?wc :remove/id ?id])]
+     [?op :assign/elem ?elem]
+     [?op :assign/value ?value]
+     (not [?op :remove? true])]
 
     [(has-value? ?elem) (current-value ?elem ?wc)]
 
@@ -89,31 +81,58 @@
 
     ;; Output
 
-    [(result ?ctr1 ?ctr2 ?value)
-     [?id1 :id/ctr ?ctr1]
-     [?id2 :id/ctr ?ctr2]
+    [(result ?id1 ?id2 ?value)
+     ;; (-id ?id1 ?ctr1 ?wc)
      (next-visible ?id1 ?id2)
      (current-value ?id2 ?value)]
     ])
 
 ;; query
 
-(def q '[:find ?ctr1 ?ctr2 ?value :where (result ?ctr1 ?ctr2 ?value)])
+(def q '[:find ?id1 ?id2 ?value :where (result ?id1 ?id2 ?value)])
+;; (def q1 '[:find ?id ?ctr ?node :where (-id ?id ?ctr ?node)])
+;; (def q2 '[:find ?id ?value :where (current-value ?id ?value)])
+;; (def q3 '[:find ?id1 ?id2 :where (next-visible ?id1 ?id2)])
+;; (def q4 '[:find ?id1 ?id2 :where (next-elem ?id1 ?id2)])
+;; (def q5 '[:find ?id :where (has-value? ?id)])
 
 ;; test
 
 (comment
 
   (def conn (create-conn "ws://127.0.0.1:6262"))
-
-  (exec! conn (register-query db "rga" q rules))
+  (stream/consume #(println %) (bus/subscribe (:out conn) :out))
 
   (exec! conn
-    (transact db [{:db/id 1 :assign/time 4 :assign/key 100 :assign/value "X"}
-                  {:db/id 2 :assign/time 2 :assign/key 100 :assign/value "Y"}])
+    (register-query db "q_result" q rules)
+    ;; (register-query db "q_-id" q1 rules)
+    ;; (register-query db "q_current-value" q2 rules)
+    ;; (register-query db "q_next-visible" q3 rules)
+    ;; (register-query db "q_next-elem" q4 rules)
+    ;; (register-query db "q_has-value" q5 rules)
+    )
 
-    (transact db [{:db/id 4 :assign/time 10 :assign/key 100 :assign/value "Z"}
-                  {:db/id 5 :assign/time 10 :assign/key 200 :assign/value "Z"}])
-
-    (transact db [{:db/id 3 :assign/time 6 :assign/key 200 :assign/value "Y"}]))
+  (exec! conn
+    (transact db [[:db/add [1 0] :insert/after [0 0]]
+                  [:db/add [2 0] :insert/after [0 0]]
+                  [:db/add [3 0] :insert/after [2 0]]
+                  [:db/add [4 0] :insert/after [1 0]]
+                  [:db/add [5 0] :insert/after [2 0]]
+                  [:db/add [6 0] :insert/after [2 0]]])
+    (transact db [{:db/id [0 1] :assign/elem [0 0] :assign/value "-"}
+                  {:db/id [2 1] :assign/elem [2 0] :assign/value "H"}
+                  {:db/id [6 1] :assign/elem [6 0] :assign/value "e"}
+                  {:db/id [6 2] :assign/elem [6 0] :assign/value "i"}
+                  [:db/add [6 2] :remove? true]
+                  {:db/id [5 1] :assign/elem [5 0] :assign/value "l"}
+                  [:db/add [5 1] :remove? true]
+                  {:db/id [5 3] :assign/elem [5 0] :assign/value "y"}
+                  {:db/id [3 1] :assign/elem [3 0] :assign/value "l"}
+                  [:db/add [3 1] :remove? true]
+                  {:db/id [1 1] :assign/elem [1 0] :assign/value "o"}
+                  {:db/id [1 2] :assign/elem [1 0] :assign/value "i"}
+                  [:db/add [1 1] :remove? true]
+                  [:db/add [1 2] :remove? true]
+                  {:db/id [4 1] :assign/elem [4 0] :assign/value "!"}
+                  {:db/id [4 2] :assign/elem [4 0] :assign/value "?"}]))
   )
