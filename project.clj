a3a0aee73ec957c25ae5d78c71e1a90e626ccd9b
niko <me@nikolasgoebel.com>
2019-08-31 19:08:00 +0200

    Update project.clj for Clojars publication

--- project.clj
+++ project.clj
@@ -1,5 +1,9 @@
-(defproject clj-3df "0.1.0"
+(defproject com.sixthnormal/clj-3df "0.1.0-alpha"
   :description "3DF client"
+  :url "https://github.com/sixthnormal/clj-3df"
+  :license {:name "Eclipse Public License"
+            :url  "https://www.eclipse.org/legal/epl-v20.html"}
+
   :plugins [[lein-tools-deps "0.4.1"]]
   :middleware [lein-tools-deps.plugin/resolve-dependencies-with-deps-edn]
   :lein-tools-deps/config {:config-files ["deps.edn" :install :user :project]}
